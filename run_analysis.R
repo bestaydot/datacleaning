ddd1629108f95fb749587f9e52bcd50d0f4328a5
Ayorinde <oluwatoyin@Olareds-MacBook-Pro.local>
2022-12-08 21:10:43 +0100

    unzip data

---
+++ run_analysis.R
@@ -0,0 +1,4 @@
+#unzip the download file
+zipFile <- "/Users/oluwatoyin/Desktop/Coursera/R/datacleaning/Dataset.zip"
+outDir <- "/Users/oluwatoyin/Desktop/Coursera/R/datacleaning"
+unzip(zipFile, exdir = outDir)
